7fec14a9706072437ddbdde2b5b545f252646b27
siobhan <sr@saibhreas.com>
2021-06-17 17:21:51 -0400

    fdsfa

--- db/index.js
+++ db/index.js
@@ -2,8 +2,7 @@ const connection = require("./connection");
 
 class DB {
   // Keeping a reference to the connection on the class in case we need it later
-  constructor(connection) {
-    this.connection = connection;
+  constructor(cohis.connection = connection;
   }
 
   // Find all employees, join with roles and departments to display their roles, salaries, departments, and managers
