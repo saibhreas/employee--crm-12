4b7d11434c028bf12bcda44fe33c92eba016e02f
siobhan <sr@saibhreas.com>
2021-06-19 14:30:04 -0400

    added video have to just get out of this

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 
 # Unit 12 MySQL Homework: Employee Tracker
 
+### Video Link:
+https://drive.google.com/file/d/1eD2ebt9gVCWJP-ClcnewWpRX2PMXA3V4/view?usp=sharing
+
 Developers are often tasked with creating interfaces that make it easy for non-developers to view and interact with information stored in databases. Often these interfaces are known as **C**ontent **M**anagement **S**ystems. In this homework assignment, your challenge is to architect and build a solution for managing a company's employees using node, inquirer, and MySQL.
 
 ## Instructions
--- db/seed.sql
+++ db/seed.sql
@@ -3,32 +3,45 @@ use employees;
 INSERT INTO department
     (name)
 VALUES
+    ('HR'),
     ('Sales'),
     ('Engineering'),
     ('Finance'),
-    ('Legal');
+    ('Legal'),
+    ('Oceanic'),
+    ('Travel'),
+    ('Maintenaiance');
+  
+
+
+
 
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('Sales Lead', 100000, 1),
-    ('Salesperson', 80000, 1),
-    ('Lead Engineer', 150000, 2),
-    ('Software Engineer', 120000, 2),
-    ('Account Manager', 160000, 3),
-    ('Accountant', 125000, 3),
-    ('Legal Team Lead', 250000, 4),
-    ('Lawyer', 190000, 4);
+    ('Sales Lead', 100000, 2),
+    ('Salesperson', 80000, 2),
+    ('Lead Engineer', 150000, 3),
+    ('Software Engineer', 120000, 3),
+    ('Account Manager', 160000, 2),
+    ('Accountant', 125000, 4),
+    ('Legal Team Lead', 250000, 5),
+    ('Lawyer', 190000, 5),
+    ('CEO', 300000,''),
+    ('Galcial Observation', 50000, 6);
+    
 
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('John', 'Doe', 1, NULL),
+    ('John', 'Doe', 2, NULL),
     ('Mike', 'Chan', 2, 1),
     ('Ashley', 'Rodriguez', 3, NULL),
     ('Kevin', 'Tupik', 4, 3),
     ('Kunal', 'Singh', 5, NULL),
     ('Malia', 'Brown', 6, 5),
     ('Sarah', 'Lourd', 7, NULL),
-    ('Tom', 'Allen', 8, 7);
+    ('Tom', 'Allen', 8, 7),
+    ('Alice', 'Wonderland', 11, 1)
+    ('Siobhan', 'Known', 12, NULL);
 
